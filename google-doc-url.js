ab2661fef79db18d47c74cfb2191910c6e3aede9
MateuszKarwo <103440010+MateuszKarwo@users.noreply.github.com>
2022-04-11 17:34:19 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = https://docs.google.com/spreadsheets/d/107igd0RHYzaIo1-4eT3ViJWKyFyAFjSSgo1nADD29oY/edit#gid=0
+var googleDocURL = https://docs.google.com/spreadsheets/d/107igd0RHYzaIo1-4eT3ViJWKyFyAFjSSgo1nADD29oY/edit?usp=sharing
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
